f5191b0dd1b35a0b254fd7ef0704d714f4bc7212
BigMacCheese <juandamor45@gmail.com>
2024-10-02 23:06:08 -0500

    commit restaurant page view model

--- lib/viewmodels/restaurant_page_view_model.dart
+++ lib/viewmodels/restaurant_page_view_model.dart
@@ -2,27 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:test3/models/food.dart';
 import 'package:test3/services/restaurant_api_service.dart';
 
-class FoodViewModel with ChangeNotifier {
+class FoodViewModel extends ChangeNotifier {
   final ApiService _apiService = ApiService();
   List<Food> menu = [];
 
-  bool _isLoading = false;
-  bool get isLoading => _isLoading;
-
   Future<void> loadMenu(int restaurantId) async {
-    _isLoading = true;
-    notifyListeners(); // Notificamos que estamos cargando
-
     try {
       menu = await _apiService.fetchFoods(restaurantId);
     } catch (e) {
       print('Error fetching menu: $e');
     } finally {
-      _isLoading = false;
-      // Usar addPostFrameCallback para evitar el error
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        notifyListeners();
-      });
+      notifyListeners();
     }
   }
 }
