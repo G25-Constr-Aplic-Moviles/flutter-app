094d31016667c315c0c16e758aee8730e7516fe2
MussoV <s.rodriguez52@uniandes.edu.co>
2024-10-13 18:25:41 -0500

    login integration
    integrated login(working)

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,13 @@ import 'package:test3/pages/login_page.dart';
 import 'package:test3/viewmodels/nearby_restaurants_viewmodel.dart';
 import 'package:test3/viewmodels/restaurants_list_viewmodel.dart';
 import 'package:test3/viewmodels/route_view_model.dart';
-import 'package:test3/viewmodels/restaurant_page_viewmodel.dart';
+//import 'package:test3/viewmodels/restaurant_page_viewmodel.dart';
 import 'package:test3/viewmodels/LoginViewModel.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'pages/login_page.dart';
 
 void main() async {
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: 'test.env');
   runApp(const MyApp());
 }
 
--- lib/viewmodels/restaurant_page_view_model.dart
+++ lib/viewmodels/restaurant_page_view_model.dart
@@ -1,18 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:test3/models/food.dart';
-import 'package:test3/services/restaurant_api_service.dart';
+import 'package:test3/services/api_service.dart';
 
-class FoodViewModel extends ChangeNotifier {
-  final ApiService _apiService = ApiService();
-  List<Food> menu = [];
-
-  Future<void> loadMenu(int restaurantId) async {
-    try {
-      menu = await _apiService.fetchFoods(restaurantId);
-    } catch (e) {
-      print('Error fetching menu: $e');
-    } finally {
-      notifyListeners();
-    }
-  }
-}
