4e1e4dc6f5b4d50b100de5613b12887c1967d3bf
s.murciag <s.murciag@uniandes.edu.co>
2024-10-14 13:34:27 -0500

    Overflow fixed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -14,128 +14,136 @@ class LoginView extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(255, 82, 71, 1),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/logo.png',
-              width: 150,
-              height: 150,
-            ),
-            Container(
-              padding: const EdgeInsets.all(15.0),
-              height: 400,
-              width: 284,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2.0,
-                ),
-                borderRadius: BorderRadius.circular(12.0),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/logo.png',
+                width: 150,
+                height: 150,
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Login to your account',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                height: 400,
+                width: 284,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
                   ),
-                  const SizedBox(height: 40),
-                  const Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Email Address'),
-                    ],
-                  ),
-                  TextField(
-                    onChanged: (value) {
-                      loginViewModel.email = value;
-                    },
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'example@email.com',
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Password'),
-                    ],
-                  ),
-                  TextField(
-                    onChanged: (value) {
-                      loginViewModel.password = value;
-                    },
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter your password',
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: 284,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        loginViewModel.login(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromRGBO(183, 40, 31, 1),
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero,
-                        ),
-                      ),
-                      child: loginViewModel.isLoading
-                          ? const CircularProgressIndicator(
-                        color: Colors.white,
-                      )
-                          : const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Login to your account',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                       ),
                     ),
-                  ),
-                  if (loginViewModel.errorMessage.isNotEmpty)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Text(
-                        loginViewModel.errorMessage,
-                        style: const TextStyle(color: Colors.red),
-                      ),
-                    ),
-                  const SizedBox(height: 10),
-                  RichText(
-                    text: TextSpan(
-                      text: 'New to GastroAndes? ',
-                      style: const TextStyle(
-                          color: Colors.black, fontFamily: 'Gidugu'),
-                      children: [
-                        TextSpan(
-                          text: 'Sign up',
-                          style: const TextStyle(
-                              color: Colors.blue, fontFamily: 'Gidugu'),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => RegisterPage()),
-                              );
-                            },
-                        ),
+                    const SizedBox(height: 40),
+                    const Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Email Address'),
                       ],
                     ),
-                  ),
-                ],
+                    TextField(
+                      onChanged: (value) {
+                        loginViewModel.email = value;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'example@email.com',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Password'),
+                      ],
+                    ),
+                    TextField(
+                      onChanged: (value) {
+                        loginViewModel.password = value;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter your password',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: 284,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          loginViewModel.login(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromRGBO(183, 40, 31, 1),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          ),
+                        ),
+                        child: loginViewModel.isLoading
+                            ? const CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                            : const Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    if (loginViewModel.errorMessage.isNotEmpty)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 2),
+                        child: Text(
+                          loginViewModel.errorMessage,
+                          style: const TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    const SizedBox(height: 10),
+                    RichText(
+                      text: TextSpan(
+                        text: 'New to GastroAndes? ',
+                        style: const TextStyle(
+                            color: Colors.black, fontFamily: 'Gidugu'),
+                        children: [
+                          TextSpan(
+                            text: 'Sign up',
+                            style: const TextStyle(
+                              color: Colors.blue,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => RegisterPage()),
+                                );
+                              },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+
   }
 }
