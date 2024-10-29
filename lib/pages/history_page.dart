f5d13d9e09f0ffcc8fa3d696f2e97ac27b1bdb7d
s.murciag <s.murciag@uniandes.edu.co>
2024-10-29 12:27:38 -0500

    Functional history page

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,64 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:test3/viewmodels/history_viewmodel.dart';
+import 'package:intl/intl.dart';
+import 'package:test3/services/history_service.dart';
+import 'package:test3/services/api_service.dart';
+import 'package:test3/models/restaurant_model.dart';
 
-class HistoryPage extends StatelessWidget {
+class HistoryPage extends StatefulWidget {
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  final HistoryService _historyService = HistoryService();
+  final ApiService _apiService = ApiService();
+  List _history = [];
+  Map<int, Restaurant> _restaurants = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchHistory();
+  }
+
+  Future<void> _fetchHistory() async {
+    List history = await _historyService.fetchHistory();
+    Map<int, Restaurant> restaurants = {};
+
+    for (var entry in history) {
+      int restaurantId = entry['restaurant_id'];
+      if (!restaurants.containsKey(restaurantId)) {
+        var restaurant = await _apiService.fetchRestaurant(restaurantId);
+        if (restaurant != null) {
+          restaurants[restaurantId] = restaurant;
+        }
+      }
+    }
+
+    setState(() {
+      _history = history;
+      _restaurants = restaurants;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final historyViewModel = Provider.of<HistoryViewModel>(context);
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(255, 82, 71, 1),
-        title: const Text(
-          'History',
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        ),
+        title: Text('Historial de Visitas'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            if (historyViewModel.isLoading)
-              const Center(
-                child: CircularProgressIndicator(),
-              )
-            else if (historyViewModel.history.isEmpty)
-              const Center(
-                child: Text(
-                  'No history records available.',
-                  style: TextStyle(fontSize: 18, color: Colors.grey),
-                ),
-              )
-            else
-              Expanded(
-                child: ListView.builder(
-                  itemCount: historyViewModel.history.length,
-                  itemBuilder: (context, index) {
-                    final item = historyViewModel.history[index];
-                    return Column(
-                      children: [
-                        ListTile(
-                          title: Text(
-                            '${item.restaurantName} - ${item.date}',
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        ),
-                        if (index != historyViewModel.history.length - 1)
-                          const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 16.0),
-                            child: Divider(
-                              color: Colors.grey,
-                              height: 1.0,
-                            ),
-                          ),
-                      ],
-                    );
-                  },
-                ),
-              ),
-          ],
-        ),
+      body: _history.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: _history.length,
+        itemBuilder: (context, index) {
+          var entry = _history[index];
+          var restaurant = _restaurants[entry['restaurant_id']];
+          var restaurantName = restaurant?.name ?? 'Desconocido';
+          var visitDate = DateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'").parseUTC(entry['timestamp']).toLocal();
+
+          return ListTile(
+            title: Text(restaurantName),
+            subtitle: Text('Visitado el ${visitDate.day}/${visitDate.month}/${visitDate.year}'),
+          );
+        },
       ),
     );
   }
--- lib/viewmodels/history_viewmodel.dart
+++ lib/viewmodels/history_viewmodel.dart
@@ -1,60 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:test3/models/history_entry.dart';
-import 'package:test3/models/token_manager.dart';
-import 'package:test3/services/api_service.dart';
 import 'package:test3/services/history_service.dart';
-import 'dart:async';
-import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:test3/viewmodels/restaurants_list_viewmodel.dart';
 
 class HistoryViewModel extends ChangeNotifier {
-  final HistoryService historyService;
-  final ApiService apiService;
-  List<HistoryEntry> _history = [];
+  final HistoryService _historyService = HistoryService();
+  final RestaurantsListViewModel _restaurantsViewModel = RestaurantsListViewModel();
 
-  bool isLoading = false;
-  String errorMessage = '';
+  List _history = [];
+  List _restaurants = [];
+  bool _isLoading = true;
 
-  HistoryViewModel({required this.historyService, required this.apiService});
+  List get history => _history;
+  List get restaurants => _restaurants;
+  bool get isLoading => _isLoading;
 
-  List<HistoryEntry> get history => _history;
+  HistoryViewModel() {
+    fetchHistory();
+  }
 
-  Future<void> getHistory(BuildContext context) async {
-    isLoading = true;
+  Future<void> fetchHistory() async {
+    _isLoading = true;
     notifyListeners();
 
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.none) {
-      isLoading = false;
-      errorMessage = 'No internet connection!';
-      notifyListeners();
-      return;
-    }
+    _history = await _historyService.fetchHistory();
 
-    try {
-      final historyList = await historyService.fetchHistory().timeout(const Duration(seconds: 5));
-      List<HistoryEntry> updatedHistory = [];
+    await _restaurantsViewModel.fetchRestaurants();
+    _restaurants = _restaurantsViewModel.restaurants;
 
-      for (var entry in historyList) {
-        final restaurant = await apiService.fetchRestaurant(entry['restaurant_id']);
-        if (restaurant != null) {
-          updatedHistory.add(HistoryEntry(
-            restaurantName: restaurant.name,
-            date: entry['date'],
-          ));
-        }
-      }
-
-      _history = updatedHistory;
-      isLoading = false;
-      errorMessage = '';
-    } on TimeoutException catch (_) {
-      isLoading = false;
-      errorMessage = 'Request timed out. Please try again.';
-    } catch (e) {
-      isLoading = false;
-      errorMessage = 'Server Error. Retry!';
-    }
-
+    _isLoading = false;
     notifyListeners();
   }
 }
