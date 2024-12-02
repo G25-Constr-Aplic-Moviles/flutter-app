b166f051d02abe88619340b55cfcf5864d7199d3
Sebastián <s.murciag@uniandes.edu.co>
2024-12-02 16:12:58 -0500

    Dish Card updated

--- lib/pages/dishes_list_view.dart
+++ lib/pages/dishes_list_view.dart
@@ -29,8 +29,6 @@ class _FoodListViewState extends State<FoodListView> {
         setState(() {
           _isConnected = true;
         });
-        // Recargar la lista de platos cuando se recupere la conexión
-        Provider.of<MenuItemViewModel>(context, listen: false).fetchAllMenus();
       } else {
         setState(() {
           _isConnected = false;
@@ -57,103 +55,121 @@ class _FoodListViewState extends State<FoodListView> {
       appBar: AppBar(
         title: Text('Lista de Platos', style: TextStyle(fontSize: 28)),
       ),
-      body: _isConnected
-          ? Consumer<MenuItemViewModel>(
+      body: Consumer<MenuItemViewModel>(
         builder: (context, menuItemViewModel, child) {
-          if (menuItemViewModel.allFoodMenu.isEmpty) {
-            return Center(child: CircularProgressIndicator());
+          if (!_isConnected && menuItemViewModel.allFoodMenu.isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.wifi_off, size: 100, color: Colors.red),
+                  SizedBox(height: 20),
+                  Text(
+                    'No Internet Connection',
+                    style: TextStyle(fontSize: 24, color: Colors.red),
+                  ),
+                ],
+              ),
+            );
           } else {
-            // Ordenar los platos alfabéticamente por nombre
-            menuItemViewModel.allFoodMenu.sort((a, b) => a['food'].name.compareTo(b['food'].name));
-            return ListView.builder(
-              itemCount: menuItemViewModel.allFoodMenu.length,
-              itemBuilder: (context, index) {
-                var item = menuItemViewModel.allFoodMenu[index];
-                Food food = item['food'];
-                String restaurantName = item['restaurantName'];
-                String restaurantAddress = item['restaurantAddress'];
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 15), // Espacio de separación más pequeño
-                  child: ListTile(
-                    contentPadding: EdgeInsets.all(20),
-                    leading: CachedNetworkImage(
-                      imageUrl: food.imageUrl,
-                      placeholder: (context, url) => CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
-                      width: 120, // Tamaño fijo para las imágenes
-                      height: 120, // Tamaño fijo para las imágenes
-                      fit: BoxFit.cover,
-                    ),
-                    title: Text(
-                      food.name,
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+            return Column(
+              children: [
+                if (!_isConnected)
+                  Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        Icon(Icons.wifi_off, color: Colors.white),
+                        SizedBox(width: 10),
                         Text(
-                          food.description.length > 50
-                              ? '${food.description.substring(0, 50)}...'
-                              : food.description,
-                          style: TextStyle(fontSize: 18),
-                        ),
-                        if (food.description.length > 50)
-                          GestureDetector(
-                            onTap: () {
-                              showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  title: Text(food.name, style: TextStyle(fontSize: 24)),
-                                  content: Text(food.description, style: TextStyle(fontSize: 18)),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () => Navigator.pop(context),
-                                      child: Text('Close', style: TextStyle(fontSize: 18)),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                            child: Text(
-                              'Show more',
-                              style: TextStyle(color: Colors.blue, fontSize: 18),
-                            ),
-                          ),
-                        SizedBox(height: 5),
-                        Text(
-                          NumberFormat.currency(locale: 'es_CO', symbol: '\$').format(food.price), // Formato de precio en Colombia
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          'Restaurant: $restaurantName',
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                        Text(
-                          'Address: $restaurantAddress',
-                          style: TextStyle(fontSize: 18),
+                          'No Internet Connection',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
                         ),
                       ],
                     ),
                   ),
-                );
-              },
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: menuItemViewModel.allFoodMenu.length,
+                    itemBuilder: (context, index) {
+                      var item = menuItemViewModel.allFoodMenu[index];
+                      Food food = item['food'];
+                      String restaurantName = item['restaurantName'];
+                      String restaurantAddress = item['restaurantAddress'];
+                      return Card(
+                        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 15), // Espacio de separación más pequeño
+                        child: ListTile(
+                          contentPadding: EdgeInsets.all(20),
+                          leading: CachedNetworkImage(
+                            imageUrl: food.imageUrl,
+                            placeholder: (context, url) => CircularProgressIndicator(),
+                            errorWidget: (context, url, error) => Icon(Icons.fastfood, size: 50, color: Colors.grey), // Ícono relacionado con comida
+                            width: 120, // Tamaño fijo para las imágenes
+                            height: 120, // Tamaño fijo para las imágenes
+                            fit: BoxFit.cover,
+                          ),
+                          title: Text(
+                            food.name,
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                food.description.length > 50
+                                    ? '${food.description.substring(0, 50)}...'
+                                    : food.description,
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              if (food.description.length > 50)
+                                GestureDetector(
+                                  onTap: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) => AlertDialog(
+                                        title: Text(food.name, style: TextStyle(fontSize: 24)),
+                                        content: Text(food.description, style: TextStyle(fontSize: 18)),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () => Navigator.pop(context),
+                                            child: Text('Close', style: TextStyle(fontSize: 18)),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                  child: Text(
+                                    'Show more',
+                                    style: TextStyle(color: Colors.blue, fontSize: 18),
+                                  ),
+                                ),
+                              SizedBox(height: 5),
+                              Text(
+                                NumberFormat.currency(locale: 'es_CO', symbol: '\$').format(food.price), // Formato de precio en Colombia
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                'Restaurant: $restaurantName',
+                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                              ),
+                              Text(
+                                'Address: $restaurantAddress',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             );
           }
         },
-      )
-          : Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.wifi_off, size: 100, color: Colors.red),
-            SizedBox(height: 20),
-            Text(
-              'No Internet Connection',
-              style: TextStyle(fontSize: 24, color: Colors.red),
-            ),
-          ],
-        ),
       ),
     );
   }
